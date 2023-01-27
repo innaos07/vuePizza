b92666d92704d8275501b9120f7f70603a97f9d2
innaos07 <samosudova07@gmail.com>
2023-01-27 17:33:47 +0100

    added helpers, layouts, views folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,19 @@
 <template>
+  <app-layout>
+    <home-view />
+  </app-layout>
+</template>
+
+<script setup>
+import AppLayout from "@/layouts/AppLayout.vue";
+import HomeView from "@/views/HomeView.vue";
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/app.scss";
+</style>
+
+<!-- <template>
   <div class="main__wrapper">
     <div class="main__header">
       <img src="@/assets/img/logo.svg" width="300" height="47" alt="V!U!E! Pizza" />
@@ -59,4 +74,4 @@ body {
   }
 }
 
-</style>
+</style> -->
